afaf41035645cc7adc2809211de9b6e7e8dfd337
Ishita Singh <2114ishita@gmail.com>
2025-07-03 10:19:18 +0530

    Added Navigation Bar

---
+++ Backend/index.jsx
@@ -0,0 +1,5 @@
+export const navlinks = [
+    {id: 'lists', title: 'Lists'},
+    {id: 'groups', title: 'Groups'},
+    {id: 'more', title: 'More'}
+];
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import { navlinks } from '../../Backend/index';
+import { useGSAP } from '@gsap/react' ;
+import { ScrollTrigger } from 'gsap/all';
+import gsap from "gsap";
+
+export default function Navbar() {
+    useGSAP(() => {
+        const timeline = gsap.timeline({
+            ScrollTrigger: {
+                trigger: 'nav', // tag ka naam h
+                start: 'bottom top',
+            }
+        })
+        timeline.fromTo('nav', {
+            backgroundColor: 'transparent'
+        }, {
+            backgroundColor: '#FFF6E3BF', // custom fade
+            backgoundFilter: 'blur(10px)',
+            duration: 1,
+            ease:'power1.inOut'
+        })
+    });
+
+  return (
+    <nav className='sticky top-0 py-3 px-10'>
+        <div className="navbar flex flex-col md:flex-row md:justify-between items-center gap-2">
+            <button className="font-cinzel text-3xl hover:cursor-pointer hover:scale-101">🪷SANGRAH</button>
+            <ul className='flex-center gap-5 text-lg'>
+                {navlinks.map((item) => (
+                    <li key={item.id} className='hover:text-[#FFCC00] hover:scale-102'>
+                        <a href={`${item.id}`}>
+                            {item.title}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </nav>
+  )
+}
