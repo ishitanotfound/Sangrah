a7bee06d66499704a4f53f4ff7b7bfda7b066f15
Ishita Singh <2114ishita@gmail.com>
2025-07-08 09:22:03 +0530

    Added Group feature(front-end only)

--- src/components/groups/GroupView.jsx
+++ src/components/groups/GroupView.jsx
@@ -3,29 +3,36 @@ import { Link } from "react-router-dom";
 export default function GroupView() {
   return (
     <section className="all-lists-in-group relative min-h-screen flex flex-col items-start px-4 sm:px-10">
-        <div className="main-content absolute mt-20">
-            <p className="text-xl text-[#FF7601] hover:cursor-default pl-2 mb-5">All lists</p>
-            <div className="all-lists-box flex flex-wrap">
-                {[...Array(5)].map((_, i) => (
-                    <Link key={i} to='/listShow'>
-                        
-                        {/* box-1 */}
-                        <div className="box-1">
-                            <div className="card card bg-[#fff]/30 rounded-3xl w-72 sm:w-85 shadow-sm p-5 m-2">
-                                <div className="card-body flex flex-col gap-1">
-                                    <h2 className="card-title text-2xl text-gray-700 font-semibold">List title</h2>
-                                    <h3>Start: 08/07/25</h3>
-                                    <h3>End: 14/07/25</h3>
-                                    <h3>By: Babu</h3>
-                                    <progress className="progress progress-success w-56" value="40" max="100"></progress>
-                                </div>
-                            </div>
-                        </div>
+      <div className="main-content mt-24 w-full">
+        {/* Title */}
+        <p className="text-xl text-[#FF7601] hover:cursor-default pl-1 sm:pl-10 mt-2">
+          All lists
+        </p>
 
-                    </Link>
-                ))}                
-            </div>
+        {/* Cards Container */}
+        <div className="all-lists-box w-full pt-4 sm:px-10 flex flex-wrap justify-center sm:justify-start gap-4">
+          {[...Array(5)].map((_, i) => (
+            <Link key={i} to="/listShow" className="w-full sm:w-auto">
+              {/* Box */}
+              <div className="card bg-[#fff]/30 rounded-3xl shadow-sm p-5 sm:p-5 w-full sm:w-85 max-w-xs">
+                <div className="card-body flex flex-col gap-1">
+                  <h2 className="card-title text-xl sm:text-2xl text-gray-700 font-semibold">
+                    List title
+                  </h2>
+                  <h3 className="text-sm sm:text-base">Start: 08/07/25</h3>
+                  <h3 className="text-sm sm:text-base">End: 14/07/25</h3>
+                  <h3 className="text-sm sm:text-base">By: Babu</h3>
+                  <progress
+                    className="progress progress-success w-full"
+                    value="40"
+                    max="100"
+                  ></progress>
+                </div>
+              </div>
+            </Link>
+          ))}
         </div>
+      </div>
     </section>
-  )
+  );
 }
