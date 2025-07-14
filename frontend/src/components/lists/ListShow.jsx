54a3cfc34673451a49ade31964912116e052e662
Ishita Singh <2114ishita@gmail.com>
2025-07-14 13:38:02 +0530

    Final fixes and enhancements

--- frontend/src/components/lists/ListShow.jsx
+++ frontend/src/components/lists/ListShow.jsx
@@ -22,7 +22,7 @@ export default function ListShow() {
 
     const fetchQuote = async () => {
       try {
-        const res = await fetch('http://localhost:5000/api/gitaquote'); // or your deployed backend URL
+        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/gitaquote`);
         const data = await res.json();
         setGitaQuote(data);
       } catch (err) {
